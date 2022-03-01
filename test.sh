124303fa53d831c27e0a7dbffd0dcc4afb406402
oyugik29 <100418451+oyugik29@users.noreply.github.com>
2022-03-01 11:56:15 +0300

    (tests): added card ops tests

--- test.sh
+++ test.sh
@@ -1,2 +1,10 @@
 curl http://0.0.0.0:9900
+curl -u test:test -d "param1=test&param2=test" http://0.0.0.0:9900/post  
 curl -u test:test http://0.0.0.0:9900/json 
+curl -u test:test -d "param1=test&param2=test" http://0.0.0.0:9900/debit  
+curl -u test:test -d "param1=test&param2=test" http://0.0.0.0:9900/credit 
+curl -u test:test -d "param1=test&param2=test" http://0.0.0.0:9900/create
+curl -u test:test -d "param1=test&param2=test" http://0.0.0.0:9900/getCardDetails
+curl -u test:test -d "param1=test&param2=test" http://0.0.0.0:9900/block
+curl -u test:test -d "param1=test&param2=test" http://0.0.0.0:9900/unblock  
+
