682c82db2e09f9c1dc9353c16d2a36baf1cafc42
oyugik29 <100418451+oyugik29@users.noreply.github.com>
2022-03-01 11:55:22 +0300

    (feat): added card ops routes

--- routes.go
+++ routes.go
@@ -3,11 +3,18 @@ package main
 import (
     "encoding/json"
     "io"
+    "log"
     "net/http"
 )
 
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Welcome, hubuc\n")
+    io.WriteString(w, "hello, hubuc\n")
+}
+
+func HandlePost(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    log.Println(r.PostForm)
+    io.WriteString(w, "12345\n")
 }
 
 type Result struct {
@@ -19,3 +26,40 @@ func HandleJSON(w http.ResponseWriter, r *http.Request) {
     result, _ := json.Marshal(Result{"12345"})
     io.WriteString(w, string(result))
 }
+
+func HandleDebit(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    log.Println(r.PostForm)
+    io.WriteString(w, "debit\n")
+}
+
+func HandleCredit(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    log.Println(r.PostForm)
+    io.WriteString(w, "credit\n")
+}
+
+func HandleCreate(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    log.Println(r.PostForm)
+    io.WriteString(w, "create\n")
+}
+
+func HandleGetCardDetails(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    log.Println(r.PostForm)
+    io.WriteString(w, "getCardDetails\n")
+}
+
+func HandleBlock(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    log.Println(r.PostForm)
+    io.WriteString(w, "block\n")
+}
+
+func HandleUnblock(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    log.Println(r.PostForm)
+    io.WriteString(w, "unblock\n")
+}
+
