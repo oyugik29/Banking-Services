5db127ceb0f3cedb0d637eff63fcca2b708d2111
oyugik29 <100418451+oyugik29@users.noreply.github.com>
2022-03-01 14:15:33 +0300

    (feat): added log file and file based logging

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
     "log"
     "net/http"
+    "os"
 )
 
 func main() {
-handleRequests()
+    file, err := openLog("bank.log")
+    if err != nil{
+        log.Fatal(err)
+    }
+    log.SetOutput(file)
+    log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+    log.Println("logFile Created")
+
+    handleRequests()
 
     log.Fatal(http.ListenAndServe(":9900", nil))
 }
@@ -25,6 +34,12 @@ func handleRequests(){
     http.HandleFunc("/block", PostOnly(basicAuth(HandleBlock)))
     http.HandleFunc("/unblock", PostOnly(basicAuth(HandleUnblock)))
     http.HandleFunc("/getCards", GetOnly(basicAuth(HandleGetCards)))
+}
 
-
+func openLog(path string) (*os.File, error){
+    logFile, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+    if err != nil{
+        return nil, err
+    }
+    return logFile, nil
 }
